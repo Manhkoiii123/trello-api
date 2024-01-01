d8bd2115f365f043a7d2b7894e2d8fcf369a17b4
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-01 20:04:15 +0700

    ev setup

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,13 +1,11 @@
-const MONGODB_URI =
-  "mongodb+srv://manhtranduc0202:manhtranduc@cluster0mannhtd.pgvp7it.mongodb.net/?retryWrites=true&w=majority";
-const DATABASE_NAME = "trello-manhtd";
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "~/config/environment";
 
 //taọ 1 ins lúc đầu là null do ch kết nối db => tạo ra tí nối nó dễ
 let trelloDatabaseInstance = null;
 
 //khoởi tạo 1 clinet hưu trên docs
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -17,7 +15,7 @@ const mongoClientInstance = new MongoClient(MONGODB_URI, {
 export const CONNECT_DB = async () => {
   await mongoClientInstance.connect();
   //kết nối thành công thì lấy ra db theo name và gán ó lại biến trelloDatabaseInstance
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
 };
 //hcir gọi hàm này khi đã kết ối thành công tới môngdb
 export const GET_DB = () => {
