d7caa9af01cc0d8aad42b583fcab2bdf5f17ed58
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-01 21:21:23 +0700

    learn router

--- src/server.js
+++ src/server.js
@@ -3,15 +3,10 @@ import express from "express";
 import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
 import exitHook from "async-exit-hook";
 import { env } from "~/config/environment";
+import { APIs_V1 } from "~/routes/v1";
 const START_SERVER = () => {
   const app = express();
-
-  app.get("/", async (req, res) => {
-    // hàm listco là hàm trả ra 1 promise nên cần có aw
-    // console.log(await GET_DB().listCollections().toArray());
-    res.end("<h1>Hello World!</h1><hr>");
-  });
-
+  app.use("/v1", APIs_V1);
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(
       `Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`
