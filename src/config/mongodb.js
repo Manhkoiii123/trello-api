3d3490d495a3e971cff8db56eae5703c72aab041
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-01 18:08:16 +0700

    connnect db

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,26 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const MONGODB_URI =
+  "mongodb+srv://manhtranduc0202:manhtranduc@cluster0mannhtd.pgvp7it.mongodb.net/?retryWrites=true&w=majority";
+const DATABASE_NAME = "trello-manhtd";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+//taọ 1 ins lúc đầu là null do ch kết nối db => tạo ra tí nối nó dễ
+let trelloDatabaseInstance = null;
+
+//khoởi tạo 1 clinet hưu trên docs
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+  //kết nối thành công thì lấy ra db theo name và gán ó lại biến trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+//hcir gọi hàm này khi đã kết ối thành công tới môngdb
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error("Must connect to Db first");
+  return trelloDatabaseInstance;
+};
