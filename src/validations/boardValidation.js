d6b3d3de7c3d5222fb9ceb1e46571a1723c4f5dc
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-02 17:50:52 +0700

    error handle

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     //tạo post chỉ tạo bằng title và des thôi
@@ -32,9 +33,15 @@ const createNew = async (req, res, next) => {
     });
     next();
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message, //new Error(error) trả về string luôn dễ lấy
-    });
+    const errorMessage = new Error(error).message;
+    const customError = new ApiError(
+      StatusCodes.UNPROCESSABLE_ENTITY,
+      errorMessage
+    );
+    next(customError);
+    // res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+    //   errors: new Error(error).message, //new Error(error) trả về string luôn dễ lấy
+    // });
   }
 };
 export const boardValidation = { createNew };
