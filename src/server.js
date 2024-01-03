7d08967fab305c5c321fc943aa3a0e40a787cdb2
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-03 19:39:45 +0700

    cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 import express from "express";
+import cors from "cors";
 import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
 import exitHook from "async-exit-hook";
 import { env } from "~/config/environment";
 import { APIs_V1 } from "~/routes/v1";
+
 import { errorHandlingMiddleware } from "~/middlewares/errorHandlingMiddleware";
+import { corsOptions } from "./config/cors";
 const START_SERVER = () => {
   const app = express();
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use("/v1", APIs_V1);
 
