865c0465c5029bb50e97f2750405efceffbd7632
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-05 21:58:15 +0700

    add card column api

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -1,4 +1,6 @@
 import Joi from "joi";
+import { ObjectId } from "mongodb";
+import { GET_DB } from "~/config/mongodb";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
 
 // Define Collection (name & schema)
@@ -20,8 +22,42 @@ const CARD_COLLECTION_SCHEMA = Joi.object({
   updatedAt: Joi.date().timestamp("javascript").default(null),
   _destroy: Joi.boolean().default(false),
 });
-
+const validateBeforCreate = async (data) => {
+  return await CARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforCreate(data);
+    const newCardToAdd = {
+      ...validData,
+      boardId: new ObjectId(validData.boardId),
+      columnId: new ObjectId(validData.columnId),
+    };
+    const createdCard = await GET_DB()
+      .collection(CARD_COLLECTION_NAME)
+      .insertOne(newCardToAdd);
+    return createdCard;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+const findOneById = async (id) => {
+  try {
+    const res = await GET_DB()
+      .collection(CARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id), //cái id ày phải là ObjectId
+      });
+    return res;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 export const cardModel = {
   CARD_COLLECTION_NAME,
   CARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById,
 };
