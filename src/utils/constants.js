0624fc841ecce961fc2d4d67e13dc031efc7b3a8
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-01-06 23:43:52 +0700

    deployv2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
   //ko cần localhost nữa
-  "https://trello-web-xi.vercel.app/",
+  "https://trello-web-umber.vercel.app",
 ];
 export const BOARD_TYPES = {
   PUBLIC: "public",
