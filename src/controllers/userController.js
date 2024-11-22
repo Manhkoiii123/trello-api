6c940188bd9d9f05b00b5da85ae8141a627a449c
padi-dev-manhtd <manhtd@paditech.com>
2024-11-22 21:26:15 +0700

    http only( bên fe có cái credental tại cái interceptor mới dùng được cái này)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
+import ms from "ms";
 import { userService } from "~/services/userService";
 const { StatusCodes } = require("http-status-codes");
-
 const createNew = async (req, res, next) => {
   try {
     const createdUser = await userService.createNew(req.body);
@@ -20,6 +20,18 @@ const verifyAccount = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const result = await userService.login(req.body);
+    res.cookie("refreshToken", result.refresh_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: ms("14 days"),
+    });
+    res.cookie("accessToken", result.access_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: ms("14 days"),
+    });
     res.status(StatusCodes.OK).json(result);
   } catch (error) {
     next(error);
